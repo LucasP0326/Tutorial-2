da393c0f33eb474a9e8537b4cd2a296a3b609e80
Lucas Sanchez <36286172+LucasP0326@users.noreply.github.com>
2022-03-19 01:17:32 -0400

    Add files via upload

---
+++ EnemyMover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+
+    public float speed = 1.0f;
+
+    private float startTime;
+
+    private float journeyLength;
+
+    void Start()
+    {
+        startTime = Time.time;
+
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+
+        float fracJourney = distCovered / journeyLength;
+
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
+    }
+}
